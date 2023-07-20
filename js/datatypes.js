d444bb0a007da8dfc185b76ee47bd218ca7bb9b6
Asumdev <musayahayamusa123@gmail.com>
2023-07-20 11:21:25 -0700

    First Commit

---
+++ js/datatypes.js
@@ -0,0 +1,47 @@
+var str = "Musa";
+
+alert(typeof(str));
+
+var n = true;
+
+alert(typeof(n));
+
+var u = undefined;
+
+alert(typeof(u));
+
+var b = null;
+
+alert(typeof(n));
+
+
+
+
+//Declaring an empty variable
+
+var a =;
+var b =;
+var k =;
+var t =;
+
+//Declaring an  variable
+
+var name =  "Musa";
+var Gender = "Male";
+var age = 26;
+var lastName ="Yahaya";
+
+var name =  "Musa";
+var lastName ="Yahaya";
+var maritalStatus = "Single";
+var age = 26;
+
+
+var name =  "Musa"; var lastName ="Yahaya";  var maritalStatus = "Single"; var age = 26;
+
+
+var myAge = "I am 25 years old";
+var yourAge = "You are 30 years old";
+
+console.log(myAge);
+console.log(yourAge);
